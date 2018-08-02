15cd84b5a367f1bad1a164c8277b0b6cde591d74
fmostasso <fmostasso@yahoo.com.br>
2018-08-02 15:05:59 -0300

    comando delete - não funcionando

--- class/Usuario.php
+++ class/Usuario.php
@@ -128,6 +128,20 @@ class Usuario {
 		));
 	}
 
+	public function delete(){
+
+		$sql = new Sql();
+
+		$sql->query("DELETE FROM tb_usuarios WHERE idusuario = :ID", array(
+			':ID'=>$this->getIdusuario()
+		));
+
+		$this->setIdsuario(0);
+		$this->setDeslogin("");
+		$this->setDessenha("");
+		$this->setDtcadastro(new DateTime());
+	}
+
 	public function __construct($login = "", $password = ""){
 
 		$this->setDeslogin($login);
